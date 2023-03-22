1feb393e4365587d086ede7e2f7cc658da9943f8
raveena-synsoft <56683864+raveena-synsoft@users.noreply.github.com>
2023-03-22 15:16:54 +0530

    Change in package.json to  upgrade packages
    Implemented swagger

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   // const app = await NestFactory.create(AppModule);
   const app = await NestFactory.create<NestExpressApplication>(
     AppModule,
   );
-  app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
-
-app.useStaticAssets(join(__dirname, '..', 'public'));
-app.setBaseViewsDir(join(__dirname, '..', 'views'));
-app.setViewEngine('hbs');
+  
+  const config = new DocumentBuilder()
+  .setTitle('Fleet Management API')
+  .setDescription('The Fleet management API Swagger Doc')
+  .setVersion('1.0')
+  .addTag('Fleet')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(4000);
 }
 bootstrap();
